c258e27b00d7711f81128e4e629cb073fb919ca0
Nik-portfolio <kravtcovmikita@gmail.com>
2022-07-31 21:10:41 +0300

    fix: bag with shift

---
+++ backspace.js
@@ -0,0 +1,24 @@
+document.addEventListener('click', (event) => {
+  if(event.target.innerText === 'Backspace') {
+    let selectionStart = input.selectionStart
+    if(input.selectionStart != 0) {
+      input.value = input.value.slice(0,selectionStart-1)   + input.value.slice(selectionStart)
+    } else {
+      input.value = input.value.slice(0,selectionStart-1)
+    }
+    input.selectionStart = selectionStart-1
+    input.selectionEnd = selectionStart-1
+  }
+})
+document.addEventListener('keydown', (event) => {
+  if(event.key === 'Backspace') {
+    let selectionStart = input.selectionStart
+    if(input.selectionStart != 0) {
+      input.value = input.value.slice(0,selectionStart-1)   + input.value.slice(selectionStart)
+    } else {
+      input.value = input.value.slice(0,selectionStart-1)
+    }
+    input.selectionStart = selectionStart-1
+    input.selectionEnd = selectionStart-1
+  }
+})
--- delete.js
+++ delete.js
@@ -8,30 +8,7 @@ document.addEventListener('click', (event) => {
       console.log(selectionStart, input.selectionStart)
   }
 })
-document.addEventListener('click', (event) => {
-  if(event.target.innerText === 'Backspace') {
-    let selectionStart = input.selectionStart
-    if(input.selectionStart != 0) {
-      input.value = input.value.slice(0,selectionStart-1)   + input.value.slice(selectionStart)
-    } else {
-      input.value = input.value.slice(0,selectionStart-1)
-    }
-    input.selectionStart = selectionStart-1
-    input.selectionEnd = selectionStart-1
-  }
-})
-document.addEventListener('keydown', (event) => {
-  if(event.key === 'Backspace') {
-    let selectionStart = input.selectionStart
-    if(input.selectionStart != 0) {
-      input.value = input.value.slice(0,selectionStart-1)   + input.value.slice(selectionStart)
-    } else {
-      input.value = input.value.slice(0,selectionStart-1)
-    }
-    input.selectionStart = selectionStart-1
-    input.selectionEnd = selectionStart-1
-  }
-})
+
 document.addEventListener('keydown', (event) => {
   if(event.key === 'Delete') {
     let selectionStart = input.selectionStart;
